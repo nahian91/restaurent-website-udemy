9c022b6fcb12f6dda6f0da2ba505924b2cc43e9f
nahian28 <nahian.instructory@gmail.com>
2022-09-20 13:18:13 +0600

    M

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.slider-area').slick({
+    autoplay: true,
+    prevArrow: false,
+    nextArrow: false,
+    dots: false
+});
+
+$('.testimonials').slick({
+    autoplay: true,
+    slidesToShow: 2,
+    dots: true,
+    arrows: false
+});
